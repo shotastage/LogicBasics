887334c359a4a2b276748459a1fbe8bccdbea51e
shotastage <shota.shimazu@magicalsoft.app>
2024-01-17 01:38:40 +0900

    first commit

---
+++ Tensorl/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Tensorl
+//
+//  Created by Shota Shimazu on 2024/01/16.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
